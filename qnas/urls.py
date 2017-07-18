76f788b604470dc4447f62dfd24af15942fffde6
PW486 <ooqwe486@gmail.com>
2017-07-18 09:30:14 +0900

    Incheon Airport

--- qnas/urls.py
+++ qnas/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index)
+    url(r'^q/create/$', views.create),
+    url(r'^q/create_question/$', views.create_question),
 ]
--- qnas/views.py
+++ qnas/views.py
@@ -2,7 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 
 # Create your views here.
 def index(request):
-    return render(request, 'qnas/index.html')
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'qnas/index.html', context)
+
+def create(request):
+    return render(request, 'qnas/create.html')
+
+def create_question(request):
+    title = request.POST['title']
+    return HttpResponseRedirect("/")
